83cdd9291065c83210c6dab67f2837faa1e5b270
Lasha Tsnobiladze <tsnobiladzelasha@gmail.com>
2023-03-22 21:47:35 +0400

    finish but here is some little problem

---
+++ script.js
@@ -0,0 +1,59 @@
+var a = document.querySelector("#main-after1");
+var b = document.querySelector("#main-after2");
+var c = document.querySelector("#main-after3");
+var d = document.querySelector("#main-after4");
+var e = document.querySelector("#main-after5");
+function openBoxA() {
+ 
+      if(a.style.display === "none"){
+        a.style.display = "block"
+    }else{
+        a.style.display = "none"
+    }
+    return openBoxA;
+
+}
+function openBoxB() {
+ 
+    if(b.style.display === "none"){
+      b.style.display = "block"
+  }else{
+      b.style.display = "none"
+  }
+  return openBoxB;
+
+}
+function openBoxC() {
+ 
+    if(c.style.display === "none"){
+      c.style.display = "block"
+  }else{
+      c.style.display = "none"
+  }
+  return openBoxC;
+
+}
+function openBoxD() {
+ 
+    if(d.style.display === "none"){
+      d.style.display = "block"
+  }else{
+      d.style.display = "none"
+  }
+  return openBoxD;
+
+}
+function openBoxE() {
+ 
+    if(e.style.display === "none"){
+      e.style.display = "block"
+  }else{
+      e.style.display = "none"
+  }
+  return openBoxE;
+
+}
+
+
+
+
